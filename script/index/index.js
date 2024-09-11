97d57d016408d7d5b9ec9b7e827a81d627e700b7
João Pedro Lisboa Ribeiro Sampaio <nobrejp@outlook.com>
2024-09-11 15:00:02 -0300

    Criando keyframes

--- script/index/index.js
+++ script/index/index.js
@@ -1,5 +1,21 @@
 //
 
+
+const navOnHover =() =>{
+let op = document.getElementsByClassName('divGeral');
+
+op[0].style=" justify-content: center;flex-wrap: wrap;display: inline-flex;margin-left:70px;margin-right: auto;height: 200px;width: 600px;display: inline-flex;border-bottom: 2px solid ;"
+
+
+}
+
+const navOutHover =() =>{
+    let op = document.getElementsByClassName('divGeral');
+    op[0].style=" justify-content: center;flex-wrap: wrap;display: inline-flex;margin-left:70px;margin-right: auto;height: 200px;width: 600px; display:display: inline-flex;border-bottom: 2px solid ;"
+
+
+}
+
 const  navFunction = (nav) => {
 let container = document.getElementById('container-main');
 if(document.documentElement.scrollTop >= 238 ){
